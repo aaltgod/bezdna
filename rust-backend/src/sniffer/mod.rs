fc79420acc95c04cdb336ce6c31d6d01c1e1e08c
Daniil Komarov <dankomarov@ozon.ru>
2024-05-12 02:00:31 +0300

    [ALPHA-3] feat: add data saving

--- rust-backend/src/sniffer/mod.rs
+++ rust-backend/src/sniffer/mod.rs
@@ -1,48 +1,34 @@
 use std::collections::HashMap;
 use std::io::Error;
 use std::ops::Sub;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::time;
 
 use pnet::{
     datalink::Channel::Ethernet,
-    packet::{
-        ethernet::{EthernetPacket, EtherTypes},
-        ipv4::Ipv4Packet,
-        Packet,
-    },
+    packet::{ethernet::EthernetPacket, ipv4::Ipv4Packet, Packet},
 };
-use pnet::datalink::{bpf, ChannelType, Config, EtherType};
+use pnet::datalink::bpf;
 use pnet::packet::ethernet::EtherTypes::Ipv4;
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::PrimitiveValues;
 use pnet::packet::tcp::{TcpFlags, TcpPacket};
-use sqlx::{Pool, Postgres};
-use tokio::select;
-use tokio::sync::mpsc::Receiver;
+use regex::bytes;
+use tokio::sync::Mutex;
 
-use crate::domain::PacketDirection;
+use crate::domain;
+use crate::domain::FlagDirection;
+use crate::repository::db::postgres::flags as flags_repo;
+use crate::repository::db::postgres::streams as streams_repo;
+use crate::sniffer::external_types::PORTS_TO_SNIFF;
+use crate::sniffer::internal_types::{PortPair, TcpPacketInfo};
 
-lazy_static! {
-    static ref PORTS_TO_SNIFF: Mutex<HashMap<u16, bool>> = Mutex::new(HashMap::new());
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct PortPair {
-    src: u16,
-    dst: u16,
-}
-
-#[derive(Debug, Clone)]
-struct TcpPacketInfo {
-    payload: String,
-    packet_direction: PacketDirection,
-    completed: bool,
-    at: chrono::DateTime<chrono::Utc>,
-}
+pub mod external_types;
+mod internal_types;
 
 pub struct Sniffer {
-    db: Pool<Postgres>,
+    streams_repo: streams_repo::Repository,
+    flags_repo: flags_repo::Repository,
     interface_name: String,
     tcp_packet_info_by_port_pair: Arc<Mutex<HashMap<PortPair, Vec<TcpPacketInfo>>>>,
     tcp_stream_ttl: chrono::Duration,
@@ -51,13 +37,15 @@ pub struct Sniffer {
 
 impl Sniffer {
     pub fn new(
-        db: Pool<Postgres>,
+        streams_repo: streams_repo::Repository,
+        flags_repo: flags_repo::Repository,
         interface_name: &str,
         tcp_stream_ttl: chrono::Duration,
         max_stream_ttl: chrono::Duration,
     ) -> Self {
         Sniffer {
-            db,
+            streams_repo,
+            flags_repo,
             interface_name: interface_name.to_string(),
             tcp_packet_info_by_port_pair: Arc::new(Mutex::new(HashMap::new())),
             tcp_stream_ttl,
@@ -65,7 +53,7 @@ impl Sniffer {
         }
     }
 
-    pub async fn run(self, mut ports_to_watch_rx: Receiver<u16>) -> Result<(), Error> {
+    pub async fn run(self) -> Result<(), Error> {
         let arc_self = Arc::new(self);
         let self_handler = Arc::clone(&arc_self);
         let self_manager = Arc::clone(&arc_self);
@@ -87,27 +75,17 @@ impl Sniffer {
         ) {
             Ok(Ethernet(tx, rx)) => (tx, rx),
             Ok(_) => panic!("INVALID type"),
-            Err(e) => panic!("{e}"),
+            Err(e) => panic!("{}", e),
         };
 
         futures_util::future::join_all(vec![
             tokio::spawn(async move {
                 loop {
-                    select! {
-                        Some(msg) = ports_to_watch_rx.recv() => {
-                            tracing::info!("{}",msg);
-
-                            let mut m = PORTS_TO_SNIFF.lock().unwrap();
-                            m.insert(msg, true);
-                        },
-                    }
-                }
-            }),
-            tokio::spawn(async move {
-                loop {
                     match rx.next() {
                         Ok(packet) => {
-                            self_handler.handle_eth_packet(EthernetPacket::new(packet).expect("OK"))
+                            self_handler
+                                .handle_eth_packet(EthernetPacket::new(packet).expect("OK"))
+                                .await
                         }
                         Err(e) => error!("{}", e),
                     }
@@ -124,10 +102,9 @@ impl Sniffer {
         loop {
             tokio::time::sleep(time::Duration::from_secs(5)).await;
 
-            let mut tcp_packet_info_by_port_pair =
-                self.tcp_packet_info_by_port_pair.lock().unwrap();
+            let mut tcp_packet_info_by_port_pair = self.tcp_packet_info_by_port_pair.lock().await;
 
-            let mut port_pairs_to_remove: Vec<PortPair> =
+            let mut completed_stream_port_pairs: Vec<PortPair> =
                 Vec::with_capacity(tcp_packet_info_by_port_pair.len());
 
             for (port_pair, tcp_packet_info) in tcp_packet_info_by_port_pair.iter() {
@@ -137,28 +114,73 @@ impl Sniffer {
                     if time_now
                         .sub(tcp_packet_info.last().unwrap().at)
                         .gt(&self.tcp_stream_ttl)
+                        || tcp_packet_info.last().unwrap().completed
                         || time_now
                             .sub(tcp_packet_info.first().unwrap().at)
                             .gt(&self.max_stream_ttl)
                     {
                         warn!("DONE: {} {:?}", tcp_packet_info.len(), port_pair);
 
-                        port_pairs_to_remove.push(*port_pair);
+                        completed_stream_port_pairs.push(*port_pair);
                     }
                 }
             }
 
-            warn!("port_pairs_to_remove LEN: {}", port_pairs_to_remove.len());
+            warn!(
+                "completed_stream_port_pairs LEN: {}",
+                completed_stream_port_pairs.len()
+            );
 
-            for pair in port_pairs_to_remove {
+            let mut streams_to_insert: Vec<domain::Stream> =
+                Vec::with_capacity(completed_stream_port_pairs.len());
+
+            for pair in completed_stream_port_pairs {
+                let tcp_packet_info = tcp_packet_info_by_port_pair.get(&pair).unwrap();
+
+                let mut payload = String::new();
+                let mut started_at: chrono::DateTime<chrono::Utc> = Default::default();
+                let mut ended_at: chrono::DateTime<chrono::Utc> = Default::default();
+
+                for (idx, info) in tcp_packet_info.iter().enumerate() {
+                    if idx == 0 {
+                        started_at = info.at
+                    }
+
+                    if idx.eq(&(tcp_packet_info.len() - 1)) {
+                        ended_at = info.at
+                    }
+
+                    payload.push_str(info.payload.as_str())
+                }
+
+                if !payload.is_empty() {
+                    streams_to_insert.push(domain::Stream {
+                        id: 0,
+                        service_port: pair.dst,
+                        payload,
+                        started_at,
+                        ended_at,
+                    });
+                }
+
+                // TODO: удалить из памяти только после
+                // успешной вставки в БД
                 tcp_packet_info_by_port_pair.remove(&pair);
+            }
+
+            if !streams_to_insert.is_empty() {
+                self.streams_repo
+                    .insert_streams(streams_to_insert)
+                    .await
+                    .unwrap();
             }
         }
     }
 
-    fn handle_eth_packet(&self, packet: EthernetPacket) {
+    async fn handle_eth_packet(&self, packet: EthernetPacket<'_>) {
         if packet.get_ethertype() == Ipv4
             // TODO: почему-то на loopback'е не возвращает тип протокола.
+            // Поэтому обрабатываем этот момент так.
             || packet.get_ethertype().to_primitive_values().0 == 0
         {
             let ipv4_packet = Ipv4Packet::new(packet.payload()).unwrap();
@@ -166,17 +188,17 @@ impl Sniffer {
             if ipv4_packet.get_next_level_protocol() == IpNextHeaderProtocols::Tcp {
                 let tcp_packet = TcpPacket::new(ipv4_packet.payload()).unwrap();
 
-                self.handle_tcp_packet(tcp_packet);
+                self.handle_tcp_packet(tcp_packet).await;
             }
         }
     }
 
-    fn handle_tcp_packet(&self, packet: TcpPacket) {
+    async fn handle_tcp_packet(&self, packet: TcpPacket<'_>) {
         let source_port = packet.get_source();
         let destination_port = packet.get_destination();
 
-        let (port_pair, packet_direction): (PortPair, Option<PacketDirection>) = {
-            let ports = PORTS_TO_SNIFF.lock().unwrap();
+        let (port_pair, packet_direction): (PortPair, Option<FlagDirection>) = {
+            let ports = PORTS_TO_SNIFF.lock().await;
 
             if ports.contains_key(&destination_port) {
                 (
@@ -184,7 +206,7 @@ impl Sniffer {
                         src: source_port,
                         dst: destination_port,
                     },
-                    Some(PacketDirection::IN),
+                    Some(FlagDirection::IN),
                 )
             } else if ports.contains_key(&source_port) {
                 (
@@ -192,7 +214,7 @@ impl Sniffer {
                         src: destination_port,
                         dst: source_port,
                     },
-                    Some(PacketDirection::OUT),
+                    Some(FlagDirection::OUT),
                 )
             } else {
                 return;
@@ -203,7 +225,7 @@ impl Sniffer {
             return;
         }
 
-        let mut tcp_packet_info_by_port_pair = self.tcp_packet_info_by_port_pair.lock().unwrap();
+        let mut tcp_packet_info_by_port_pair = self.tcp_packet_info_by_port_pair.lock().await;
 
         let time_now = chrono::Local::now().to_utc();
 
@@ -230,6 +252,7 @@ impl Sniffer {
                 .entry(port_pair)
                 .and_modify(|i| {
                     info.completed = true;
+
                     i.push(info)
                 });
         };
